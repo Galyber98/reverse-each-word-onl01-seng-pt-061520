8f9e930d9894fd74705d23f4f63a0077920d9045
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-27 23:55:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,12 +5,3 @@ def reverse_each_word(array)
   end
  reversed_words.join(" ")
 end
-
-
-def reverse_each_word_two(array)
-  sentence_two = "Hi again, just making sure it's reversed!".split
-  second_reversed_words = sentence_two.collect do |second|
-     second.reverse
-    end
-  second_reversed_words.join(" ")
-end
