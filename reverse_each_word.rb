31b36dc42b471b5c6c1319e6a260256551eff2e1
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-28 00:08:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   reversed_words = array.collect do |words|
     words.reverse 
-  end
- reversed_words.join("")
+  end 
+ reversed_words.join(" ")
 end
