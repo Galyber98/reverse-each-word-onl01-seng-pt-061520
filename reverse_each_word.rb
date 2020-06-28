46926d777bf27c5cc7149baf6216700e390e66b4
Galilea Bernal <galilea.bernal98@gmail.com>
2020-06-28 00:01:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,2 @@
-def reverse_each_word(string)
-  sentence = "Hello there, and how are you?".split
-  reversed_words = sentence.collect do |words|
-    words.reverse
-  end
- reversed_words.join(" ")
-end
+def reverse_each_word(sentence)
+  
